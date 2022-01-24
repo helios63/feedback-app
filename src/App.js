2e38b4888c05c0bd1366da1e88a6e2c1e2a4348c
Maxime Mondet <maxime.mondet@gmail.com>
2022-01-24 15:11:46 +0100

    Creating Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import Header from "./components/Header"
+
 function App () {
     return (
         <>
-        <div className="container">
-            <h1>My App</h1>
-        </div>
+        <Header text={'Feedback UI'} />
         </>
     )
 }
